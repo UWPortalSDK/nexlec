35daf0d2b7765890d792c213192ecf8c81eb8145
Tarun Khanna <tkhanna@uwaterloo.ca>
2017-11-23 14:08:12 -0500

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -10,13 +10,6 @@ angular.module('portalApp')
         $scope.tutTogText = $scope.showTuts ? "Hide Tutorials" : "Show Tutorials";
     };
     
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90]
-    ];
-    
 	$scope.items = [];
 	
 	// Show main view in the first column as soon as controller loads
